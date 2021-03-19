d78bea28be73da76a1a0bd49c3ec312b566e3ac3
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-03-19 21:07:54 +0600

    adding

--- FAQ/script.js
+++ FAQ/script.js
@@ -1,20 +1,7 @@
-const insert = document.getElementById('insert')
+const toggles = document.querySelectorAll('.faq-toggle')
 
-window.addEventListener('keydown',(e)=>{
-    insert.innerHTML = `
-    <div class="key">
-            ${e.key ===''?'Space' : e.key}
-            <small>
-                event.key
-            </small>
-        </div>
-        <div class="key">
-            ${e.keyCode}
-            <small>event.keyCode</small>
-        </div>
-        <div class="key">
-            ${e.code }
-            <small>event.code</small>
-        </div>
-    `
-})
+toggles.forEach(toggle => {
+    toggle.addEventListener('click',()=>{
+        toggle.parentNode.classList.toggle('active')
+    })
+});
