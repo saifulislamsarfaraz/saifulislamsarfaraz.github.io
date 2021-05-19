0953c3f13e67685ec76bc220f89993e5bf4400af
Saiful Islam Sarfraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-05-20 00:02:42 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const sliderContainer = document.querySelector('.slider-container');
-const slideRight = document.querySelector('.right-slide');
-const slideLeft = document.querySelector('.left-slide');
-const upButton = document.querySelector('.up-button');
-const downButton = document.querySelector('.down-button');
-const slidesLength = slideRight.querySelectorAll('div').length
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
 
+// Toggle nav
+toggle.addEventListener('click', () =>document.body.classList.toggle('show-nav')
+);
+
+// Show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Hide modal on outside click
+window.addEventListener('click', e =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
