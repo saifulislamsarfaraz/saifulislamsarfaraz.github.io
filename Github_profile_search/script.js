f09d42992b3ba7c25fdd2588fdc6266ac4d9d8f1
Saiful Islam Sarfraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-05-20 00:07:01 +0600

    Add files via upload

---
+++ Github_profile_search/script.js
@@ -0,0 +1,7 @@
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-slide');
+const slideLeft = document.querySelector('.left-slide');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+const slidesLength = slideRight.querySelectorAll('div').length
+
