6318f25081a20230f5a5e319182eec3ec4c4b08f
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-08 21:59:54 +0600

    Doubly linkedlist constructor adding

---
+++ Data Structure JS/Doubly-Linkedlist.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+class LinkedList{
+    constructor (value){
+        const newNode = new Node(value)
+        
+    }
+}
