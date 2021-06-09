eb91a617a90eb9ad2dc961eb8795f11b58db38b1
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-09 22:28:18 +0600

    axios add

--- Github_profile_search/script.js
+++ Github_profile_search/script.js
@@ -1,1 +1,14 @@
+const APIURL = 'https://api.github.com/users/'
 
+getUser('saifulislamsarfaraz')
+/* function getUser(username){
+    axios(APIURL + username)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+} */
+
+async function getUser( username){
+    const res = await axios(APIURL + username)
+    console.log(res.data)
+
+}
