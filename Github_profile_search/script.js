f90604d4032368538b3a7296bdc804d9169ff10c
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-09 23:12:04 +0600

    cange console log to request create user card

--- Github_profile_search/script.js
+++ Github_profile_search/script.js
@@ -1,6 +1,5 @@
 const APIURL = 'https://api.github.com/users/'
 
-getUser('saifulislamsarfaraz')
 /* function getUser(username){
     axios(APIURL + username)
     .then(res => console.log(res))
@@ -8,16 +7,56 @@ getUser('saifulislamsarfaraz')
 } */
 
 const form = document.getElementById('form')
+const main = document.getElementById('main')
+const search = document.getElementById('search')
 
 async function getUser( username){
     try{
         const {data} = await axios(APIURL + username)
 
-        console.log(data)
+
+        createUserCard(data)
     
     }catch(error){
         console.log(error)
     }
 }
 
-form.addEventListener('submit')
+function createUserCard(card){
+        const cardHTML = ` 
+        <div class="card">
+        <div>
+            <img src="https://randomuser.me/api/portraits/men/32.jpg" alt="" class="avater">
+        </div>
+    
+        <div class="user-info">
+             <h2>jhondoe</h2>
+            <ul>
+                <li>300 <strong>Followers</strong></li>
+                <li>100 <strong>Following</strong></li>
+                <li>50 <strong>Repos</strong></li>
+            </ul>
+
+              <div id="repo">
+                <a href="#" class="repo">Repo 1</a>
+                <a href="#" class="repo">Repo 2</a>
+                <a href="#" class="repo">Repo 3</a>
+              </div>
+        </div>
+    </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+form.addEventListener('submit',(e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user){
+        getUser(user)
+
+        search.value = '' 
+    }
+})
