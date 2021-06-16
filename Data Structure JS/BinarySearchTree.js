7c77be72ba46e43d57372a676da15b31b3ea4c7b
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-16 23:58:57 +0600

    BST

---
+++ Data Structure JS/BinarySearchTree.js
@@ -0,0 +1,14 @@
+class Node{
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BST{
+    constructor(value){
+        const newNode = new Node(value)
+        
+    }
+}
