305862208a2a01f625720e179f6890abb99c33cd
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-20 18:18:34 +0600

    SelectionSort

--- Data Structure JS/HashTable.js
+++ Data Structure JS/HashTable.js
@@ -32,6 +32,49 @@ class HashTable{
         }
         return undefined
     }
+
+    keys(){
+        let allKeys = []
+        for(let i = 0; i < this.dataMap.length;i++){
+            if(this.dataMap[i]){
+            for(let j = 0; j <this.dataMap[i].length; j++){
+                allKeys.push(this.dataMap[i][j][0])
+             }
+           }
+        }
+        return allKeys
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 let myHashTable = new HashTable()
---
+++ Data Structure JS/SelectionSort.js
@@ -0,0 +1,17 @@
+function selectionSort(){
+    let min 
+    for(let i = 0; i < Array.length - 1; i++){
+        min  = i
+        for(let j = i + 1; j < Array.length; j++){
+            if(array[j] < array[min]){
+                min = j
+                
+            }
+        }
+        if(i!==min){
+        let temp = array[j]
+        array[i] = array[min]
+        array[min] = temp
+        }
+    }
+}
