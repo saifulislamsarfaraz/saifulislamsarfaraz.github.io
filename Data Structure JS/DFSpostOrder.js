4d33417939d93e2cf367ba8b9b3115eea3ce347e
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-22 00:30:59 +0600

    inorder and postorder DFS

---
+++ Data Structure JS/DFSpostOrder.js
@@ -0,0 +1,75 @@
+class Node{
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BST{
+    constructor(){
+        this.root = null
+    }
+
+    insert(value){
+        const newNode = new Node(value)
+        if(this.root === null){
+            this.root = newNode
+            return this
+        }
+        let temp = this.root
+        while(true){
+            if(newNode.value === temp.value){
+                return undefined
+            }
+            if(newNode.value < temp.value){
+                if(temp.left===null){
+                    temp.left = newNode
+                    return this
+                }
+                temp = temp.left
+            }else{
+                if(temp.right===null){
+                    temp.right = newNode
+                    return this
+                }
+                temp = temp.right
+            }
+        }
+    }
+
+    contains(value){
+        if(this.root === null){
+            return undefined
+        }
+        let temp = this.root
+        while(temp){
+            if(value < temp.value){
+                temp = temp.left
+            }else if(value > temp.value){
+                temp = temp.right
+            }else{
+                return true
+            }
+        }
+        return false
+    }
+
+    DFSpostOrder() {
+        let results = []
+        function traverse(currentNode) {
+            if(currentNode.left) traverse(currentNode.left)
+            if(currentNode.right) traverse(currentNode.right)
+            results.push(currentNode.value)
+        }
+        traverse(this.root)
+        return results
+    }
+}
+
+let tree = new BST()
+tree.insert(10)
+tree.insert(12)
+tree.insert(13)
+tree.insert(14)
+tree.insert(15)
