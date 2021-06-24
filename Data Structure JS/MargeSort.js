b4c8d0612496b03892e924b55e7d56ad32fa28d1
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-24 17:30:09 +0600

    MargeSort

---
+++ Data Structure JS/MargeSort.js
@@ -0,0 +1,37 @@
+function marge(array1,array2){
+    let combined = []
+    let i = 0;
+    let j = 0;
+
+    while(i < array1.length && j < array2.length){
+        if(array1[i] < array2[j]){
+            combined.push(array1[i])
+            i++
+        }else{
+            combined.push(array2[j])
+            j++
+        }
+    }
+    while (i < array1.length){
+        combined.push(array1[i])
+        i++
+    }
+    while(j < array2.length){
+        combined.push(array2[j])
+        j++
+    }
+
+    return combined
+}
+
+function margeSort (array){
+    if(array.length===1)return array
+    let mid = Math.floor(array.length/2)
+    let left = array.slice(0, mid)
+    let right = array.slice(mid)
+
+    return marge(margeSort(left),margeSort(right))
+
+}
+
+margeSort([3,1,4,2])
