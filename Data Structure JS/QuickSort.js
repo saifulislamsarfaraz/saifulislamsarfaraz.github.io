d89acd01450ea8633df0fef5f63f2308ee6dc8a9
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-26 20:25:44 +0600

    pivotFunction

---
+++ Data Structure JS/QuickSort.js
@@ -0,0 +1,20 @@
+function swap(array, firstIndex, secondIndex){
+    let temp = array[firstIndex]
+    array[firstIndex] = array[secondIndex]
+    array[secondIndex] = temp
+}
+
+function pivot(array, pivotIndex = 0, endIndex = array.length-1){
+    let swapIndex = pivotIndex;
+    for(let i = pivotIndex+1; i < endIndex; i++){
+        if(array[i]<array[pivotIndex]){
+            swapIndex++
+            swap(array,swapIndex,i)
+        }
+    }
+    swap(array,pivotIndex,swapIndex)
+    return swapIndex
+}
+
+let myArray = [4,6,1,7,3,2,5]
+pivot(myArray)
