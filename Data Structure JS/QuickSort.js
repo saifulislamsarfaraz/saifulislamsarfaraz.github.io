27d37cdba2de3e31ea9660b6ac3c22ec17037fdc
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-26 21:01:34 +0600

    QuickSort Function

--- Data Structure JS/QuickSort.js
+++ Data Structure JS/QuickSort.js
@@ -16,5 +16,13 @@ function pivot(array, pivotIndex = 0, endIndex = array.length-1){
     return swapIndex
 }
 
+function quickSort(array, left = 0, right = array.length-1){
+    if(left<right){
+    let pivotIndex = pivot(array,left,right)
+    quickSort(array,left,pivotIndex-1)
+    quickSort(array,pivotIndex+1,right)
+    }
+    return array
+}
 let myArray = [4,6,1,7,3,2,5]
 pivot(myArray)
