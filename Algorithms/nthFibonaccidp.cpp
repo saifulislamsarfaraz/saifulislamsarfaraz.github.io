0ddef0e8bb283d0b9bd94d32d182d072ef511da5
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-29 19:44:09 +0600

    print all fibonacci

--- Algorithms/nthFibonaccidp.cpp
+++ Algorithms/nthFibonaccidp.cpp
@@ -18,3 +18,30 @@ int main(){
     cout<<""<<nth<<endl;
 
 }
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int memo[10005];
+int f(int n){
+    if(n<=1) return n;
+
+    if(memo[n]!=-1){
+            printf("%d\n",memo[n]);
+            return memo[n];
+    }
+    return memo[n] = f(n-1) + f(n-2);
+
+}
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 0; i<20; i++){
+        memo[i] = -1;
+    }
+    int nth = f(n);
+    cout<<""<<nth<<endl;
+
+}
+*/
+
