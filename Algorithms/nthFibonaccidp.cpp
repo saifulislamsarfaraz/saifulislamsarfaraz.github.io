7174374116e79314a9496f5e594377bc420984bf
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-29 19:45:59 +0600

    even fibonacci

--- Algorithms/nthFibonaccidp.cpp
+++ Algorithms/nthFibonaccidp.cpp
@@ -43,5 +43,34 @@ int main(){
     cout<<""<<nth<<endl;
 
 }
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int memo[10005];
+int f(int n){
+    if(n<=1) return n;
+
+    if(memo[n]!=-1){
+        if(memo[n]%2==0)
+            printf("%d\n",memo[n]);
+            return memo[n];
+    }
+    return memo[n] = f(n-1) + f(n-2);
+
+}
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 0; i<20; i++){
+        memo[i] = -1;
+    }
+    int nth = f(n);
+    cout<<""<<nth<<endl;
+
+}
+
 */
+
+
 
