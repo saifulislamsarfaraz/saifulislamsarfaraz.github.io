614cfcb8d368598f8314e61cc4d42bfa62d0f098
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-29 19:38:20 +0600

    nthFibonacci

---
+++ Algorithms/nthFibonaccidp.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int memo[10005];
+int f(int n){
+    if(n==0) return 0;
+    if(n==1) return 1;
+
+    if(memo[n]!=-1)return memo[n];
+    return memo[n] = f(n-1) + f(n-2);
+}
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 0; i<20; i++){
+        memo[i] = -1;
+    }
+    int nth = f(n);
+    cout<<""<<nth<<endl;
+
+}
