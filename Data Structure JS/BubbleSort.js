ea345891b951558159ea76dacdf42bb16d1be94d
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-06-30 23:33:57 +0600

    change

--- Data Structure JS/BubbleSort.js
+++ Data Structure JS/BubbleSort.js
@@ -2,8 +2,8 @@ function bubbleSort(array){
     for(let i = array.length-1; i > 0; i--){
         for(let j = 0; j < i; j++){
             if(array[j]>array[j+1]){
-                let temp = array[i]
-                array[i] = array[j+1]
+                let temp = array[j]
+                array[j] = array[j+1]
                 array[j+1] = temp
             }
         }
@@ -12,3 +12,147 @@ function bubbleSort(array){
 }
 
 bubbleSort(1,6,5,4,2,1)
+
+
+
+function bubbleSort(array){
+    for(let i = array.length-1; i>0; i--){
+        for(let j = 0; j < i; j++){
+            if(array[j]>array[j+1]){
+                let temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = array[j]
+            }
+        }
+    }
+}
+
+
+function bubbleSort(array){
+    for(let i = array.length - 1; i > 0; i--){
+        for(let j = 0; j < i; j++){
+            if( array[j] > array[j+1]){
+                let temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = array[j]
+            }
+        }
+    }
+    return array
+}
+
+
+function bubbleSort(array){
+    for(let i = array.length; i>0; i--){
+        for(let j = 0; j < i; i++){
+            if(array[j]>array[j+1]){
+                let temp = array[j]
+                arrayp[j] = array[j+1]
+                array[j +1] = temp
+            }
+        }
+    }
+
+    return temp
+}
+
+function bubbleSort(array){
+    for(let i = array.length - 1; i > 0; i--){
+        for (j = 0; j < i; j++){
+            if (array[j] > array[j+1]){
+                let temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+    }
+
+    return array
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
