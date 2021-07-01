c2964d9d3d961ea6ca3a24c0ab0e2878634f5fb5
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-07-01 23:27:51 +0600

    php start

---
+++ PHP/hello.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+      
+
+        $characterName = "Robin";
+        $characterAge = 35;
+
+        echo "There once was a man name ${characterName} <br>";
+        echo "He was ${characterAge} years old <br>";
+        echo "He realy liked the name ${characterName} <br>";
+        echo "but didnot being like being ${characterAge} <br>";
+
+    ?>
+
+</body>
+</html>
