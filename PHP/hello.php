e87848e90a194b15506ba37111bcaf254f7f9c00
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-07-02 23:03:14 +0600

    functions PHP

--- PHP/hello.php
+++ PHP/hello.php
@@ -16,7 +16,7 @@
     <br>
     <input type="submit">
     </form>
--->
+
     <form action="hello.php" method="get">
     Number 1:<input type="number" name="num1">
     <br><br>
@@ -29,18 +29,152 @@
 
     <input type="submit">
     </form>    
+
+    
+    <form action="hello.php" method="post">
+    
+    Color: <input type="text" name="color"> <br>
+    Plural Noun: <input type="text" name="pluralNoun"> <br>
+    Celebrity: <input type="text" name= "celebrity"> <br>
+
+    <input type="submit">
+    </form>
+    -->
+
+    <form action="hello.php" method="post">
+        Apples: <input type="checkbox" name="fruits[]" value="apples"> <br>
+        Mangos: <input type="checkbox" name="fruits[]" value="mangos"> <br>
+        Oranges: <input type="checkbox" name="fruits[]" value="oranges"> <br>
+        Pears: <input type="checkbox" name="fruits[]" value="pears"> <br>
+        
+        <input type="submit">
+
+    
+    </form>
+
+    <form action="hello.php" method="post">
+    
+    <input type="text" name="student">
+
+    <input type="submit">
+    
+    </form>
+
     <br><br><br>
     <?php
-      
-     echo $_GET["num1"] + $_GET["num2"];
+
+    $fruits = $_POST["fruits"];
+
+    echo $fruits[1];
+
+    $grades = array("Alu"=>"A+","Mula"=>"A-","Dheros"=>"C+");
+    echo $grades[$_POST["student"]];
+
+    function sayHi($name){
+        echo "hello $name";
+    }
+    $name = "saiful";
+    sayHi($name);
+
+
+    function cube($num){
+        return $num * $num * $num;
+    }
+
+    echo $cubeResult = cube(2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //$city = array("dhaka", "khulna", "Bagherhat","Noyakhali", "chandpur");
+    //echo $city[0];
+    //echo "<br>";
+    //echo count($city);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /*
+      $color = $_POST["color"];
+      $pluralNoun = $_POST["pluralNoun"];
+      $celebrity = $_POST["celebrity"];
+
+      echo "Rose are {$color} <br>";
+      echo "{$pluralNoun} are blue <br>";
+      echo "I love {$celebrity} <br>";
+
+
+
+
+
+    // echo $_GET["num1"] + $_GET["num2"];
      
      // echo $_GET["name"];
      //echo $_GET["age"];
-
-
-
-
-
 
 
       //  $characterName = "Robin";
@@ -49,7 +183,7 @@
       //  echo "There once was a man name ${characterName} <br>";
       //  echo "He was ${characterAge} years old <br>";
       //  $characterName = "Saiful";
-      /*  $phrase = "saiful";
+      $phrase = "saiful";
         echo substr($phrase, 3, 2);
         echo str_replace("ful", "<br>faysal",$phrase);
         echo "<br>";
