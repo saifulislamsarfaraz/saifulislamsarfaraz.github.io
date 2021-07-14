24137ad51c0e3aaa7c25f04051b16b0fd31fda96
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-07-14 23:58:28 +0600

    maximumTask

---
+++ Data Structure JS/MaximumTask.js
@@ -0,0 +1,9 @@
+//sort the task in the assending order of the finising time 
+//last = 0;
+//cnt = 1;
+//for loop 0 --n01
+//if starting time > task[last].finising time 
+//cnt++
+//last = i;
+
+//return cnt;
