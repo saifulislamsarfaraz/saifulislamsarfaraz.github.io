14b45fe9c6102022b5491c637e6e17b4f66b0569
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-07-18 01:40:31 +0600

    AlmostPrime.cpp

---
+++ Algorithms/AlmostPrime.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool prime[10000001];
+void solve(int n){
+    memset(prime, true, sizeof(prime));
+    for (int i = 2; i * i <= n; i++){
+        if (prime[i] == true)
+        {
+            for (int j = i * i; j <= n; j += i)
+                prime[j] = false;
+        }
+    }
+    for(int i=2;i<=n;i++){
+        if(prime[i]==true && i*i<=n){
+            cout<<i*i<<" ";
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n<4){
+        cout<<"No Almost Prime Number"<<endl;
+    }
+    else{
+        solve(n);
+    }
+}
