57e1dcef3f5bf16a48c4ed9aa01ba2e27bec0d8d
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-08-03 00:50:14 +0600

    EditDistance with print and dp solution

--- Algorithms/EditDistance.cpp
+++ Algorithms/EditDistance.cpp
@@ -1,22 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long dp[105];
 char s1[10005],s2[10005];
 int dp[10005][10005];
+int choice[10005][10005];
 char f(int p1,int p2,int len1,int len2){
         if(s1[p1]==0)return len2 - p2;
         if(s2[p2]==0)return len1 - p1;
-        if(s1[p1]==s2[p2])return f(p1+1,p2+1,len1,len2);
+        if(dp[p1][p2]!=-1)return dp[p1][p2];
+        if(s1[p1]==s2[p2])return dp[p1][p2] = f(p1+1,p2+1,len1,len2);
         int opIns = 1 + f(p1, p2+1,len1,len2);
         int opDlt = 1 + f(p1+1,p2,len1,len2);
         int opRep = 1 + f(p1+1,p2+1,len1,len2);
         if(opIns<=opDlt && opIns<=opRep){
-            return opIns;
+            choice[p1][p2] = 1;
+            return dp[p1][p2]=opIns;
         }
-        if(opDlt<=opIns && opDlt<=opRep){
-            return opDlt;
+        else if(opDlt<=opIns && opDlt<=opRep){
+            choice[p1][p2] = 2;
+            return dp[p1][p2]=opDlt;
+        }else{
+        choice[p1][p2] = 3;
+        return dp[p1][p2] = opRep;
         }
-        return opRep;
 }
 
 int main(){
@@ -29,7 +34,40 @@ int main(){
                 dp[i][j] = -1;
             }
         }
-        cout<<""<<f(0,0,len1,len2)<<endl;
+        int out = f(0,0,len1,len2);
+        cout<<""<<out<<endl;
+
+        int pos1 = 0;
+        int pos2 = 0;
+
+
+        while(true){
+            if(pos1==len1){
+                for(int i = pos2; i<len2; i++){
+                    cout<<"insert "<<s2[i]<<" at position "<<i<<endl;
+                    pos1++;
+                }
+            }
+            else if(pos2==len2){
+                for(int i = pos1; i < len1; i++){
+                    cout<<"Delete "<<s1[i]<<" from position "<<i<<endl;
+                    pos1++;
+                }
+            }else if(s1[pos1]==s2[pos2]){
+                pos1++;
+                pos2++;
+            }else if(choice[pos1][pos2]==1){
+                    cout<<"insert "<<s2[pos2]<<" at position "<<pos2<<endl;
+                    pos2++;
+            }else if(choice[pos1][pos2]==2){
+                    cout<<"Delete "<<s1[pos1]<<" from position "<<pos1<<endl;
+                    pos1++;
+            }else{
+                cout<<"Replace "<<s1[pos1]<<" with "<<s2[pos2]<<" at position "<<pos1<<endl;
+                pos1++;
+                pos2++;
+            }
+        }
 
         return 0;
 
