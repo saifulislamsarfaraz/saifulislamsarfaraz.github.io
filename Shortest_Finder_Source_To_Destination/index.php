dd5551a4ee506322c8e77401adc5dd4bbb88d5c3
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-08-24 00:46:59 +0600

    water text

---
+++ Shortest_Finder_Source_To_Destination/index.php
@@ -0,0 +1,21 @@
+<style type="text/css">
+ 
+
+</style>
+
+<div class="header">
+    <?php 
+    
+    function set_road($x,$y,$road){
+        $mark = $road;
+        $mark[$x][$y] = 1;
+        $mark[$x+1][$y] = 1;
+        $mark[$x-1][$y] = 1;
+        $mark[$x][$y+1] = 1;
+        $mark[$x][$y-1] = 1;
+        return $mark;
+    }
+
+    ?>
+
+</div>
