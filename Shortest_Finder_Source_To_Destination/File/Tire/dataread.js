f538e20aeb5b3961547fc92d043e8423542ca337
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-08-29 01:43:28 +0600

    trie search add

---
+++ Shortest_Finder_Source_To_Destination/File/Tire/dataread.js
@@ -0,0 +1,39 @@
+const items = ["abbc","adda","ffff","gggg","hhhh","reee","sadasd"]; //array to hold all the countries
+const root = new MakeNode('\0') //making the root note
+
+
+//processing Data and adding to the items array
+processingData = async () => {
+  for (const item of items) {
+    add(item, 0, root)
+  }
+  textFieldHandler({ target: { value: '' } })
+}
+
+processingData()
+
+const textField = document.getElementById('text-field')
+const list = document.getElementById('list')
+
+textField.addEventListener('input', textFieldHandler)
+
+function handleTap(e) {
+  const text = e.innerText
+  textField.value = text
+}
+
+//function for text-field
+function textFieldHandler(e) {
+  const value = e.target.value.toLowerCase()
+  const predictions = search(value, 0, root)
+
+  list.innerHTML = ''
+
+  for (let p of predictions) {
+    p = p.charAt(0).toUpperCase() + p.slice(1)
+    list.innerHTML += `<li class="list-group-item clickable" onclick="handleTap(this)"><b>${p.substring(
+      0,
+      value.length
+    )}</b>${p.substring(value.length)}</li>`
+  }
+}
---
+++ Shortest_Finder_Source_To_Destination/File/Tire/trie.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>CSE PROJECT</title>
+    <link rel="icon" href="https://assets.stickpng.com/images/58afdad6829958a978a4a693.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="container">
+            <h1 class="top">CSE 246 - Project</h1>
+            <h2>Find shortest path your destination</h2>
+        </div>
+    </header>
+
+    <main>
+        <div class="container top">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Type Where you want to go!!!" id="text-field">
+            </div>
+            <ul id="list" class="list-group">
+            </ul>
+        </div>
+    </main>
+
+    <script src="trie.js"></script>
+    <script src="app.js"></script>
+
+</body>
+
+</html>
