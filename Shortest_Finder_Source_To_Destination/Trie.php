51fde903be8e8e038bc8a0b0138d7122c5d42e2b
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-08-29 19:49:42 +0600

    trie

--- Shortest_Finder_Source_To_Destination/Trie.php
+++ Shortest_Finder_Source_To_Destination/Trie.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+
     <title>CSE PROJECT</title>
     <link rel="icon" href="https://assets.stickpng.com/images/58afdad6829958a978a4a693.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
@@ -10,27 +11,28 @@
 </head>
 
 <body>
-
     <header>
         <div class="container">
             <h1 class="top">CSE 246 - Project</h1>
             <h2>Find shortest path your destination</h2>
         </div>
     </header>
-
     <main>
-        <div class="container top">
+        <div class="top">
             <div class="input-group mb-3">
                 <input type="text" class="form-control" placeholder="Type Where you want to go!!!" id="text-field">
             </div>
             <ul id="list" class="list-group">
             </ul>
+        <div class="grd">
+            <?php include("index.php")?>    
+            </div>
         </div>
     </main>
 
+    
     <script src="TrieScript.js"></script>
     <script src="triedataRead.js"></script>
-
 </body>
 
 </html>
