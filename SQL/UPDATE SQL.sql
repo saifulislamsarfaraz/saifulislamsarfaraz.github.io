6ce29a2a1201fd1c61e3d27e6388b25d2299abc3
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2021-10-09 11:31:24 +0600

    Mexico

---
+++ SQL/UPDATE SQL.sql
@@ -0,0 +1,9 @@
+UPDATE Customers
+SET ContactName='Alfred Schmidt',City='Frankfurt'
+WHERE CustomerID=1;
+
+
+--update mulitple records
+UPDATE Customers
+SET ContactName='Juan'
+WHERE Country='Mexico';
