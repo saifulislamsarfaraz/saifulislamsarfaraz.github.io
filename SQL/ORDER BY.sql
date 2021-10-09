79dfa4392130ff90a1323643011db2d8b55ba2d3
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2021-10-09 10:42:27 +0600

    ORDER BY

---
+++ SQL/ORDER BY.sql
@@ -0,0 +1,12 @@
+SELECT * FROM Customers
+ORDER BY Country;
+
+SELECT * FROM Customers
+ORDER BY Country DESC;
+
+SELECT * FROM Customers
+ORDER BY Country,Customers;
+
+SELECT * FROM Customers
+ORDER BY Country ASC,CustomerName DESC;
+
