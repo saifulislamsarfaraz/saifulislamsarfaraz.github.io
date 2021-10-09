4ce320362b8ce55e36c73a861e8399559420b5bd
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2021-10-09 10:26:14 +0600

    AND OR NOT operation sql

--- SQL/Start.sql
+++ SQL/Start.sql
@@ -1,2 +1,20 @@
 SELECT * FROM Customers
 WHERE Country='Germany' AND City='Berlin';
+
+SELECT * FROM Customers 
+WHERE City = 'Berlin'OR City='Munchen';
+
+SELECT * FROM Customers 
+WHERE Country= 'Germany' OR Country='Spain';
+
+
+SELECT * FROM Customers
+WHERE NOT Countr='Germany';
+
+SELECT * FROM Customers
+WHERE Country='Germany' AND (City='Berlin' OR City='Munchin');
+
+SELECT * FROM Customers
+WHERE NOT Country='Germany' AND NOT Country='USA';
+
+
