2a0a40802d12f73a4c177f76ed0e65fa50846679
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2021-10-09 11:01:25 +0600

    INsert INTO

--- SQL/SQL INSERT INTO Statment.sql
+++ SQL/SQL INSERT INTO Statment.sql
@@ -2,3 +2,7 @@
 --VALUES (value1, value2, value3, ...);
 INSERT INTO Customers(CustomerName,ContactName,Address,City,PostalCode,Country);
 VALUES('Cardinal','Tom B. Erichsen','Skagen 21','Stavanger','4006','Norway');
+
+INSERT INTO Customers(CustomerName,City,Country)
+VALUES('Cardinal','Stavenger','Norway');
+
