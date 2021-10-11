87475074b43e2317209506f0eb1f230ee4edd056
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2021-10-12 01:54:23 +0600

    Between with order by

--- SQL/BETWEN.sql
+++ SQL/BETWEN.sql
@@ -11,3 +11,11 @@ AND CategoryID NOT IN (1,2,3);
 SELECT * FROM Products
 WHERE Price BETWEEN 10 AND 20;
 AND CategoryID IN (1,2,3);
+
+SELECT * FROM Products
+WHERE ProductName BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni'
+ORDER BY ProductName;
+
+SELECT * FROM Products
+WHERE ProductName BETWEEN 'Carnavon Tigers' AND 'Mozzaralla di Giovanni'
+ORDER BY ProductName;
