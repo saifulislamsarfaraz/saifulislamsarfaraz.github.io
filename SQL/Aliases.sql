9f9b2133daa1a486634a3bb3ad22d4265fadf70c
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2021-10-12 02:32:33 +0600

    customers that have postalCode city Country

--- SQL/Aliases.sql
+++ SQL/Aliases.sql
@@ -1,2 +1,11 @@
 SELECT CustomerName AS Customer, ContactName AS [Contact Person]
 FROM Customers;
+
+
+SELECT CustomerName, Address + ', ' + PostalCode + ' ' + City + ', ' + Country AS Address
+FROM Customers;
+
+SELECT CustomerName, Address + ', ' + PostalCode + ' ' + City + ', ' + Country AS Address
+FROM Customers;
+
+
