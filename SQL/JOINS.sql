048a9caef0b56a4119dfda8c17c965d1af7cf4d2
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2021-10-13 11:38:56 +0600

    JOINS

---
+++ SQL/JOINS.sql
@@ -0,0 +1,19 @@
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
+
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+LEFT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
+
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
+
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+FULL OUTER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
