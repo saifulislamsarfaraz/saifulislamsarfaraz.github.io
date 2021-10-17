58953e2ef56b92b2e14c84020df4961b68f99b55
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2021-10-17 08:11:35 +0600

    DESC

--- SQL/Having.sql
+++ SQL/Having.sql
@@ -3,4 +3,16 @@ FROM Customers
 GROUP BY Country
 HAVING COUNT(Customer_ID)>5;
 
+SELECT COUNT(Customer_ID),Country
+FROM Customers
+GROUP BY Country
+HAVING COUNT(Customer_ID)>5
+ORDER BY COUNT(Customer_ID) DESC;
 
+
+SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM (Orders
+LEFT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID)
+GROUP BY LastName
+HAVING COUNT(Orders.OrderID) > 10
+ORDER BY COUNT(Orders.OrderID) DESC;
