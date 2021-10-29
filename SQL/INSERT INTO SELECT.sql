f5a686af0bccd53295f7946bd2b2cfa6675bc973
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-10-29 23:54:54 +0600

    INSERT INTO SELECT

---
+++ SQL/INSERT INTO SELECT.sql
@@ -0,0 +1,9 @@
+INSERT INTO Customers (CustomerName, City, Country)
+SELECT SupplierName, City, Country FROM Suppliers;
+
+
+INSERT INTO Customers (CustomerName, City, Country)
+SELECT SupplierName, City, Country FROM Suppliers;
+
+
+
